2d34273945aff018d5c5c901500375a7633515f6
Шапаренко Александр Александрович <shaparenko.aa@reliab.tech>
2020-12-05 02:57:03 +0300

    feat: create RedBlackTree

---
+++ src/main/java/utils/RedBlackTree.java
@@ -0,0 +1,17 @@
+package utils;
+
+import lombok.Data;
+
+
+@Data
+public class RedBlackTree<T extends Comparable<T>> {
+
+		public RedBlackNode<T> nil = new RedBlackNode<T>();
+		public RedBlackNode<T> root = nil;
+
+		public RedBlackTree() {
+				root.left = nil;
+				root.right = nil;
+				root.parent = nil;
+		}
+}
