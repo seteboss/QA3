081ffb54d0a0916cec901d12dd51abb7b6a4ab0c
Шапаренко Александр Александрович <shaparenko.aa@reliab.tech>
2020-12-05 02:33:26 +0300

    greenStep: testing constructor RedBlackNode without key

--- src/test/java/utils/TestRedBlackTree.java
+++ src/test/java/utils/TestRedBlackTree.java
@@ -1,5 +1,19 @@
 package utils;
 
+
+import org.junit.Assert;
+import org.junit.Test;
+
 public class TestRedBlackTree {
 
+		@Test
+		public void testConstructorNodeWithoutKey(){
+				RedBlackNode<Integer> node = new RedBlackNode<>();
+				Assert.assertEquals(0, node.color);
+				Assert.assertEquals(0,node.numLeft);
+				Assert.assertEquals(0,node.numRight);
+				Assert.assertNull(node.parent);
+				Assert.assertNull(node.left);
+				Assert.assertNull(node.right);
+		}
 }
